d2c24be00581e7dba8cfffc9928a51d7c2c7f67a
vikramrc <vikramrc@f59ea30c-60b5-443a-b847-d012216c9b56>
2013-06-28 01:02:58 +0000

    Moving to EPL from LGPL v3
    git-svn-id: https://svn.code.sf.net/p/j-interop/code/trunk@147 f59ea30c-60b5-443a-b847-d012216c9b56

--- j-interopdeps/src/rpc/security/ntlm/NtlmConnection.java
+++ j-interopdeps/src/rpc/security/ntlm/NtlmConnection.java
@@ -1,19 +1,19 @@
-/* Donated by Jarapac (http://jarapac.sourceforge.net/)
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 3.0 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110, USA
- */
+/**
+* Donated by Jarapac (http://jarapac.sourceforge.net/) and released under EPL.
+* 
+* j-Interop (Pure Java implementation of DCOM protocol)
+*     
+* Copyright (c) 2013 Vikram Roopchand
+* 
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License v1.0
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v10.html
+*
+* Contributors:
+* Vikram Roopchand  - Moving to EPL from LGPL v1.
+*  
+*/
 
 package rpc.security.ntlm;
 
